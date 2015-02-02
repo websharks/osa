7b3afd4e89976a83654a79db18ca0defb925b18b
jaswsinc <jas@wsharks.com>
2015-02-02 04:04:12 -0900

    Updating typinator bash script.

--- scripts/typinator.bash
+++ scripts/typinator.bash
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
-#-- parameter: ~/path/to/script.scpt&&arg2&&arg3&&...
+#-- parameter: ~/path/to/script.scpt,arg2,arg3,...
 
 trim(){
 	local string="$*";
 	echo "$string" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//';
 };
+tokenize_escaped_commas(){
+	local string="$*";
+	echo "$string" | sed -e 's/\\,/%%comma%%/';
+};
+restore_escaped_commas(){
+	local string="$*";
+	echo "$string" | sed -e 's/%%comma%%/,/';
+};
 
-osa=''; osaArgs=''; i=0;
+script=''; scriptArgs=(); i=0;
 
 while IFS=',' read -ra args; do
 	for arg in "${args[@]}"; do
 		i=$((i+1));
 		if [ $i == 1 ]; then
-			osa="$(trim "$arg")";
+			script="$(restore_escaped_commas "$(trim "$arg")")";
 		elif [ $i -ge 2 ]; then
-			if [ "$osaArgs" == '' ]; then
-				osaArgs[i-1]="$(trim "$arg")";
-			else osaArgs[i-1]=' '"$(trim "$arg")";
-			fi;
+			scriptArgs[i-1]="$(restore_escaped_commas "$(trim "$arg")")";
 		fi;
 	done;
-done <<< "$1";
+done <<< "$(tokenize_escaped_commas "$1")";
 
-if [ "$osa" != '' ]; then
-	osa=$(eval echo "$osa"); # ~/
-	if [ "${#osaArgs[@]}" -ge 2 ]; then
-		echo -n "$(/usr/bin/env osascript -l JavaScript "$osa" "${osaArgs[@]:1}" 2>/dev/null)";
+if [ "$script" != '' ]; then
+	scriptsDir=$(eval echo ~/library/script\ libraries/websharks/scripts);
+	if [ "${#scriptArgs[@]}" -ge 2 ]; then
+		echo -n "$(/usr/bin/env osascript -l JavaScript "$scriptsDir"/"$script" "${scriptArgs[@]:1}" 2>/dev/null)";
 	else
-		echo -n "$(/usr/bin/env osascript -l JavaScript "$osa" 2>/dev/null)";
+		echo -n "$(/usr/bin/env osascript -l JavaScript "$scriptsDir"/"$script" 2>/dev/null)";
 	fi;
 fi;
